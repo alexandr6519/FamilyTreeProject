a726cbd3a8a8051bcdbd4fbc0b945a99324fa049
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-16 18:21:00 +0700

    Update Console.java
    Correcting of cod according by SOLID

--- view/Console.java
+++ view/Console.java
@@ -1,9 +1,8 @@
-package view;
+ppackage view;
 
-import model.FamilyTreeIterator;
-import model.FileHandler;
-import model.Human;
-import model.FamilyTree;
+import model.CreateAndAddNewHuman;
+import model.FindHuman;
+import model.Sorting;
 import presenter.Presenter;
 
 import java.io.IOException;
@@ -13,34 +12,26 @@ public class Console implements View {
     private Scanner scanner;
     private Presenter presenter;
     private boolean isWorking = true;
-    private FamilyTree familyTree;
-    private FileHandler fileHandler;
 
-    public Console(FileHandler fileHandler, FamilyTree familyTree) {
+    public Console() {
         scanner = new Scanner(System.in);
-        this.fileHandler = fileHandler;
-        this.familyTree = familyTree;
     }
 
     private void printInvitationForUser() {
         System.out.println();
         String str = "Выберите операцию и введите соответствующую цифру:\n" +
-                " 1 - для создания генеалогического древа из заданного списка \n" +
+                " 1 - для печати списка генеалогического древа \n" +
                 " 2 - для записи и сохранения списка родственников древа \n" +
                 " 3 - для чтения и получения списка родственников из файла \n" +
                 " 4 - для сортировки списка родственников по выбранному параметру \n" +
                 " 5 - для добавления родственника в список древа \n" +
+                " 6 - для поиска родственника по имени \n" +
                 " 0 - для выхода из меню \n";
         System.out.println(str);
     }
 
     @Override
-    public void print(FamilyTree familyTree) {
-        familyTree.getInfoAboutFamilyTree();
-    }
-
-    @Override
-    public void start() throws IOException {
+    public void start() throws IOException, ClassNotFoundException {
         printInvitationForUser();
         while (isWorking) {
             int operationNumber = scanner.nextInt();
@@ -49,62 +40,33 @@ public class Console implements View {
                     exit();
                     break;
                 case 1:
-                    familyTree.createInitialFamilyTree();
-                    familyTree.getInfoAboutFamilyTree();
+                    presenter.printChildren();
                     printInvitationForUser();
                     break;
                 case 2:
-                    fileHandler.writeTreeInFile(familyTree);
-                    System.out.printf("Вы успешно сохранили список генеалогического древа в файл \"%s.%s\" !\n", fileHandler.getFileName(), fileHandler.getFileType());
-                    familyTree.getInfoAboutFamilyTree();
+                    presenter.writeTreeInFile();
+                    System.out.printf("Вы успешно сохранили список генеалогического древа в файл \"%s.%s\" !\n", presenter.getFileName(), presenter.getFileType());
                     printInvitationForUser();
                     break;
                 case 3:
-                    System.out.printf("Генеалогическое древо, прочитанное из файла \"%s.%s\":\n", fileHandler.getFileName(), fileHandler.getFileType());
-                    familyTree.getInfoAboutFamilyTree();
+                    System.out.printf("Генеалогическое древо, прочитанное из файла \"%s.%s\":\n", presenter.getFileName(), presenter.getFileType());
+                    presenter.readFromFile();
+                    presenter.print();
                     printInvitationForUser();
                     break;
                 case 4:
-                    System.out.println("Для сортировки по имени введите цифру 1:\n" +
-                            "для сортировки по году рождения введите цифру 2: \n" +
-                            "для сортировки по id введите цифру 3:");
-                    int sortNumber = scanner.nextInt();
-
-                    switch (sortNumber) {
-                        case 1:
-                            System.out.println("Генеалогическое древо, отсортированное по имени:");
-                            familyTree.sortByName();
-                            break;
-                        case 2:
-                            System.out.println("Генеалогическое древо, отсортированное по году рождения:");
-                            familyTree.sortByBirthday();
-                            break;
-                        case 3:
-                            System.out.println("Генеалогическое древо, отсортированное по Id:");
-                            familyTree.sortById();
-                            break;
-                        default:
-                            System.out.println("Вы ввели некорректный номер сортировки!");
-                    }
-                    familyTree.getInfoAboutFamilyTree();
+                    Sorting sorting = new Sorting(presenter, scanner);
+                    sorting.sortByParameter();
                     printInvitationForUser();
                     break;
                 case 5:
-                    System.out.println("Для добавления в список введите последовательно через запятую(без пробелов): " +
-                            "фамилия, имя, отчество, пол (м или ж), id отца, id матери, год рождения" +
-                            "(в случае отсутствия id введите -1");
-                    String humanForAdding = scanner.next();
-                    String[] arrayDataOfHuman = humanForAdding.split("[,]");
-
-                    String nameFull = String.format("%s %s %s", arrayDataOfHuman[0], arrayDataOfHuman[1], arrayDataOfHuman[2]);
-                    Human father = familyTree.getHumanById(Integer.parseInt(arrayDataOfHuman[4]));
-                    Human mother = familyTree.getHumanById(Integer.parseInt(arrayDataOfHuman[5]));
-                    int birthYear = Integer.parseInt(arrayDataOfHuman[6]);
-                    int id = familyTree.getLastId() + 1;
-                    Human humanNew = new Human(id, nameFull, familyTree.getGender(arrayDataOfHuman[3]), father, mother, birthYear);
-                    familyTree.addHuman(humanNew);
-                    familyTree.getInfoAboutFamilyTree();
-                    familyTree.printChildren();
+                    CreateAndAddNewHuman createAndAddNewHuman = new CreateAndAddNewHuman(presenter, scanner);
+                    createAndAddNewHuman.readAndAddHuman();
+                    printInvitationForUser();
+                    break;
+                case 6:
+                    FindHuman findHuman =new FindHuman(presenter, scanner);
+                    findHuman.getHumanByName();
                     printInvitationForUser();
                     break;
                 default:
@@ -114,7 +76,6 @@ public class Console implements View {
         }
     }
 
-   
     private void exit() {
         System.out.println("Работа завершена, всего доброго!");
         scanner.close();
@@ -126,4 +87,5 @@ public class Console implements View {
         this.presenter = presenter;
     }
 }
+
 
