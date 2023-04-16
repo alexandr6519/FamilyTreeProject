edf642796bc287d7f22d61df6bf338cca71dc60a
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-16 18:22:38 +0700

    Update View.java
    Correcting of cod according by SOLID

--- view/View.java
+++ view/View.java
@@ -1,14 +1,13 @@
 package view;
 
-import model.FamilyTree;
 import presenter.Presenter;
 
 import java.io.IOException;
 
 public interface View {
 
-    void print(FamilyTree familyTree);
-    void start() throws IOException;
+    void start() throws IOException, ClassNotFoundException;
 
     void setPresenter(Presenter presenter);
+
 }
