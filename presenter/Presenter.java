0b5699f9b51f93030754eb8f638b53e9d3bfa671
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-16 18:24:41 +0700

    Update Presenter.java
    Correcting of cod according by SOLID

--- presenter/Presenter.java
+++ presenter/Presenter.java
@@ -1,8 +1,11 @@
 package presenter;
 
+import model.Gender;
 import model.Human;
 import model.Service;
 import view.View;
+
+import java.io.IOException;
 
 public class Presenter {
     private View view;
@@ -14,11 +17,61 @@ public class Presenter {
         view.setPresenter(this);
     }
 
-   /* //public void addH(Human human) {        service.addHuman(human);    }
+    public void addHuman(Human human) {
+        service.addHuman(human);
+        System.out.printf("Вы успешно добавили в древо следующего родственника:\n %s \n", human.toString());
+    }
 
-    public void getRecords() {
-        String answer = service.getNotes();
-        view.print(answer);
+    public void print() {
+        service.print();
+    }
 
-    }*/
+    public String getFileName() { return service.getFileName(); }
+
+    public String getFileType() {
+        return service.getFileType();
+    }
+
+    public Human getHumanByName(String name) {
+        return service.getHumanByName(name);
+    }
+
+    public void createInitialTree() {
+        service.createInitialTree();
+    }
+
+    public void printChildren(){
+        service.printChildren();
+    }
+    public void writeTreeInFile()throws IOException {
+        service.writeTreeInFile();
+    }
+
+    public void readFromFile()throws IOException, ClassNotFoundException{
+        service.readFromFile();
+    }
+
+    public void sortByName(){
+        service.sortByName();
+    }
+
+    public void sortById(){
+        service.sortById();
+    }
+
+    public void sortByBirthYear(){
+        service.sortByBirthYear();
+    }
+
+    public Human getHumanById(int id) {
+        return service.getHumanById(id);
+    }
+
+    public int getLastId(){
+        return service.getLastId();
+    }
+
+    public Gender getGender(String gender) {
+        return service.getGender(gender);
+    }
 }
