160be2dde41e557bf832a38687162b482551b2d0
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-17 22:47:12 +0700

    Update Presenter.java
    correcting of cod 17/04/2023

--- presenter/Presenter.java
+++ presenter/Presenter.java
@@ -1,6 +1,5 @@
 package presenter;
 
-import model.Gender;
 import model.Human;
 import model.Service;
 import view.View;
@@ -17,61 +16,48 @@ public class Presenter {
         view.setPresenter(this);
     }
 
-    public void addHuman(Human human) {
-        service.addHuman(human);
-        System.out.printf("Вы успешно добавили в древо следующего родственника:\n %s \n", human.toString());
+    public Human readAndCreateHuman() {
+        return service.readAndCreateHuman();
+    }
+
+    public void getHumanByName() {
+        service.getHumanByName();
     }
 
     public void print() {
         service.print();
     }
 
-    public String getFileName() { return service.getFileName(); }
+    public String getFileName() {
+        return service.getFileName();
+    }
 
     public String getFileType() {
         return service.getFileType();
-    }
-
-    public Human getHumanByName(String name) {
-        return service.getHumanByName(name);
     }
 
     public void createInitialTree() {
         service.createInitialTree();
     }
 
-    public void printChildren(){
+    public void printChildren() {
         service.printChildren();
     }
-    public void writeTreeInFile()throws IOException {
+
+    public void writeTreeInFile() throws IOException {
         service.writeTreeInFile();
     }
 
-    public void readFromFile()throws IOException, ClassNotFoundException{
+    public void readFromFile() throws IOException, ClassNotFoundException {
         service.readFromFile();
     }
 
-    public void sortByName(){
-        service.sortByName();
+    public void sortByParameter(int sortNumber) {
+        service.sortByParameter(sortNumber);
     }
 
-    public void sortById(){
-        service.sortById();
-    }
-
-    public void sortByBirthYear(){
-        service.sortByBirthYear();
-    }
-
-    public Human getHumanById(int id) {
-        return service.getHumanById(id);
-    }
-
-    public int getLastId(){
-        return service.getLastId();
-    }
-
-    public Gender getGender(String gender) {
-        return service.getGender(gender);
+    public void addHuman(Human human) {
+        service.addHuman(human);
+        System.out.printf("Вы успешно добавили добавили в древо нового родственника:\n  %s\n", human.toString());
     }
 }
