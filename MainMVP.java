c647d586a3bdf3505e52a9db7cb0b0a07ee9f0ed
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-17 22:54:41 +0700

    Update MainMVP.java
    correcting of cod 17/04/2023

--- MainMVP.java
+++ MainMVP.java
@@ -11,10 +11,13 @@ public class MainMVP {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         FileHandler fileHandler = new FileHandler("familyTree", "out");
         FamilyTree<model.Human> familyTree = new FamilyTree<>();
+        
         Service service = new Service(familyTree, fileHandler);
         View view = new Console();
+        
         Presenter presenter = new Presenter(view, service);
         presenter.createInitialTree();
+        
         view.start();
     }
 }
