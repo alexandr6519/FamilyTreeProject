e3b1b715e2948bd98105eae2ef1717419bb8be99
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-04-17 23:04:53 +0700

    Update FamilyTree.java
    correcting of cod 17/04/2023

--- model/FamilyTree.java
+++ model/FamilyTree.java
@@ -1,4 +1,7 @@
-package model;
+
+import model.comparator.HumanComparatorByBirthday;
+import model.comparator.HumanComparatorById;
+import model.comparator.HumanComparatorByName;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -12,8 +15,84 @@ public class FamilyTree<H extends Human> implements Serializable, FamilyTreeIter
         this.familyTree = new ArrayList<H>();
     }
 
-    public List<H> getFamilyTree() {
+    List<H> getFamilyTree() {
         return familyTree;
+    }
+
+    void sortByParameter(int sortNumber) {
+        switch (sortNumber) {
+            case 1:
+                sortByName();
+                System.out.println("Генеалогическое древо отсортировано по имени");
+                break;
+            case 2:
+                System.out.println("Генеалогическое древо отсортировано по году рождения");
+                sortByBirthYear();
+                break;
+            case 3:
+                System.out.println("Генеалогическое древо отсортировано по Id");
+                sortById();
+                break;
+            default:
+                System.out.println("Вы ввели некорректный номер сортировки!");
+        }
+    }
+
+    void addHuman(H human) {
+        familyTree.add(human);
+        if (human.getMother() != null) {
+            human.getMother().addChild(human);
+        }
+        if (human.getFather() != null) {
+            human.getFather().addChild(human);
+        }
+    }
+
+    Human getHumanById(int id) {
+        for (Human human : familyTree) {
+            if (human.getId() == id) {
+                return human;
+            }
+        }
+        return null;
+    }
+
+    int getLastId() {
+        if (familyTree == null){
+            return -1;
+        }
+        return familyTree.size() - 1;
+    }
+
+    Human getHumanByName(String name) {
+        String nameFull = name.replace(",", " ");
+        for (Human human : familyTree) {
+            if (human.getFullName().equals(nameFull)) {
+                return human;
+            }
+        }
+        return null;
+    }
+
+    Gender getGender(String gender) {
+        if (gender.equals("м")) {
+            return Gender.male;
+        } else if (gender.equals("ж")){
+            return Gender.female;
+        }
+        return null;
+    }
+
+    void sortByName() {
+        familyTree.sort(new HumanComparatorByName());
+    }
+
+    void sortByBirthYear() {
+        familyTree.sort(new HumanComparatorByBirthday());
+    }
+
+    void sortById() {
+        familyTree.sort(new HumanComparatorById());
     }
 
     @Override
